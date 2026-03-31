7803fa5a8c26cb689b300a1a9210e1d6b1f66f24
之心 <173612205@qq.com>
2026-03-31 16:13:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url))
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/EnSite/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
